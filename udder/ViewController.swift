721351a33c9112cef2ab472894c27d5c9239365d
imedev4 <imedev4@imedev4s-macbook-pro.local>
2018-07-15 09:30:49 +0300

    Initial Commit

---
+++ udder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  udder
+//
+//  Created by imedev4 on 02/11/1439 AH.
+//  Copyright © 1439 5W2H. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
